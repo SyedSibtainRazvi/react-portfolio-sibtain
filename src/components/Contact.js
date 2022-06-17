537c6345ab60213dea18d9b9264222bcdc775607
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-06-17 13:30:45 +0530

    bug: added required attribute
    Prevent default submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,8 +10,8 @@ const Contact = () => {
           <p className='text-4xl font-bold inline border-b-4 border-white text-white'>Contact</p>
           <p className='text-white py-4'>Let's talk over a coffee ("")?</p>
         </div>
-        <input className='bg-[#F6F6F6] p-2 border-2' type="text" placeholder='Name' name='name' />
-        <input className='my-4 p-2 bg-[#F6F6F6] border-2' type="email" placeholder='Email' name='email' />
+        <input className='bg-[#F6F6F6] p-2 border-2' type="text" placeholder='Name' name='name' required />
+        <input className='my-4 p-2 bg-[#F6F6F6] border-2' type="email" placeholder='Email' name='email' required/>
         <textarea className='bg-[#F6F6F6] rounded-xl p-2 border-2' name="message" rows="10" placeholder='Coffee Date and Time :)'></textarea>
         <button className='text-white border-2 hover:bg-white hover:border-white px-4 py-3 my-8 mx-auto flex items-center hover:text-[#000000]'>Collaborate
           <span>
