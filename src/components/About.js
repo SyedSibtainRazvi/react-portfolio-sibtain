a265506de9ff2e541b07ac0db0c6d5ef8c964458
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-08-11 22:21:02 +0530

    doc: about

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
         </div>
         <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
           <div className='sm:text-right text-4xl font-bold '>
-            <p>Hi there! I am Syed Sibtain, a 24-year-old cool Developer, Gamer, and Writer.</p>
+            <p>Hi there! I am Syed Sibtain, a 25-year-old cool Developer, Gamer, and Writer.</p>
           </div>
           <div>
             <p>I am a passionate front-end developer with hands-on experience building web applications. I am currently living in Bangalore. A tech-savy individual who is self-motivated and a fast-paced learner with good interpersonal and organizational skills. And yes, I like to play games and code!</p>
